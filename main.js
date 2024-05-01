6c521dd50d3fb8388cf84ba481b6b2273432b9dd
Adrian Ortiz <20220681@itla.edu.do>
2024-05-01 01:27:21 -0400

    Dia_Uno_CSS

---
+++ main.js
@@ -0,0 +1,16 @@
+
+/*
+function imprimir(){ 
+
+  var content = document.getElementById("content2printges").innerHTML;
+  var printSector = window.open("", "");
+
+  printSector.document.write('<html><body><style> @media print { @page { margin: 0; } } </style>');
+  printSector.document.write(content);
+  printSector.document.write('</body></html>');
+
+  printSector.document.close();
+  printSector.print();
+  printSector.close();
+}
+*/
